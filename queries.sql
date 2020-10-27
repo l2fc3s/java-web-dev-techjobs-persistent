4764296298cdefab86d39b8bea4b0a32d18beab6
Larry LaMar <lmrlrry@gmail.com>
2020-10-26 20:22:18 -0500

    completed part 1 of assignment

--- queries.sql
+++ queries.sql
@@ -1,4 +1,10 @@
 ## Part 1: Test it with SQL
+SELECT * FROM techjobs.job;
+Columns:
+    id: int PK
+    employer: varchar(255)
+    name: varchar(255)
+    skills: varchar(255)
 
 ## Part 2: Test it with SQL
 
