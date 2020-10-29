df49cfa97b1059fdf0e4624c303df08d4eb59e10
Larry LaMar <lmrlrry@gmail.com>
2020-10-28 20:10:15 -0500

    completed Data Layer section

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,24 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @NotBlank
+    @Size(min = 3, max = 250, message="Skill must be between 3 and 150 characters.")
+    private String location;
+
+    public Skill() {}
+
+    //Getter + setter
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
