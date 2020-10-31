7c96535161ccbb272b762665fb4fdac2eae75831
Larry LaMar <lmrlrry@gmail.com>
2020-10-31 17:07:00 -0500

    completed up to HomeController section

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,11 +1,19 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
+
+    @JoinColumn
+    @OneToMany
+    private List<Job> jobs = new ArrayList<>();
 
     @NotBlank
     @Size(min = 3, max = 150, message="Location must be between 3 and 150 characters.")
