f41eb0cd9596d6dacfeef92a1f0888867b3479be
Larry LaMar <lmrlrry@gmail.com>
2020-11-06 00:20:29 -0600

    completed HomeController pt 5

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package org.launchcode.javawebdevtechjobspersistent.controllers;
 
+import org.launchcode.javawebdevtechjobspersistent.models.Employer;
 import org.launchcode.javawebdevtechjobspersistent.models.Job;
 import org.launchcode.javawebdevtechjobspersistent.models.data.EmployerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by LaunchCode
@@ -32,9 +34,16 @@ public class HomeController {
     public String displayAddJobForm(Model model) {
         model.addAttribute("title", "Add Job");
         model.addAttribute(new Job());
+
+        // Adds employer data from employerRepository into the form template (variable in templates/add)
         model.addAttribute("employers", employerRepository.findAll());
         return "add";
     }
+
+//    TODO processAddJobForm, add code inside of this method to select
+//     the employer object that has been chosen to be affiliated with
+//     the new job. You will need to select the employer using the
+//     request parameter you’ve added to the method.
 
     @PostMapping("add")
     public String processAddJobForm(@ModelAttribute @Valid Job newJob,
@@ -43,6 +52,13 @@ public class HomeController {
         if (errors.hasErrors()) {
             model.addAttribute("title", "Add Job");
             return "add";
+        }
+
+        //HomeController part 5
+        Optional<Employer> result = employerRepository.findById(employerId);
+        if (result.isPresent()) {
+            Employer employer =  result.get();
+            newJob.setEmployer(employer); //sets as selected employer
         }
 
         return "redirect:";
